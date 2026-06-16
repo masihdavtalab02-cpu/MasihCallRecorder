0ec64fa9a23301c1f312f28f53de681bc7bf94fe
masihdavtalab02-cpu <masihdavtalab02@gmail.com>
2026-06-16 12:31:23 +0330

    Add BootReceiver

---
+++ app/src/main/java/com/masih/callrecorder/receiver/BootReceiver.kt
@@ -0,0 +1,7 @@
+package com.masih.callrecorder.receiver
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
